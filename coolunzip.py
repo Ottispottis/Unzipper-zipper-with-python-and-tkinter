42bd8f2b085595ea6ae91890cbba95f29bcecd14
Ottispottis <otto.s.heldt@gmail.com>
2021-02-18 13:51:33 +0200

    Update coolunzip.py

--- coolunzip.py
+++ coolunzip.py
@@ -9,16 +9,17 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=350, height=400)
 canvas.grid(columnspan=4, rowspan=4)
 
-# Instructions
-
 instructions = tk.Label(root, text="Select a file B)", font="Raleway")
 instructions.grid(columnspan=3, column=0, row=1)
+
+# Placing logo to tkinter window
 
 logo = Image.open("cool.png")
 logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image=logo)
 logo_label.image = logo
 logo_label.grid(column=1, row=0)
+
 
 def zip_file():
     file = askopenfile(parent=root, mode="rb", filetype=[("Zip file", "*.zip")])
@@ -49,12 +50,14 @@ def unzip_file():
         textbox2.insert(1.0, text2)
         textbox2.grid(column=1,row=4)
 
-# Browse
 
-browse_str = tk.StringVar()
-browse_btn = tk.Button(root, textvariable=browse_str, command=lambda:zip_file(), font="Raleway", bg="#20bebe", fg="white", height=2, width=30)
-browse_str.set("Unzip a file B)")
-browse_btn.grid(column=1, row=2)
+# creating buttons for zipping and unzipping
+
+
+unzip_str = tk.StringVar()
+unzip_btn = tk.Button(root, textvariable=unzip_str, command=lambda:zip_file(), font="Raleway", bg="#20bebe", fg="white", height=2, width=30)
+unzip_str.set("Unzip a file B)")
+unzip_btn.grid(column=1, row=2)
 
 zip_str = tk.StringVar()
 zip_btn = tk.Button(root, textvariable=zip_str, command=lambda:unzip_file(), font="Raleway", bg="#20bebe", fg="white", height=2, width=30)
